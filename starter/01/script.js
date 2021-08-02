db795b2c62a44af4928fcd696741445af7cd7be7
seungjohan <cmostrillion@gmail.com>
2021-08-02 16:58:11 +0900

    linking js file to html and values, variables

---
+++ starter/01/script.js
@@ -0,0 +1,22 @@
+let js = 'amazing';
+if (js === 'amazing') alert('JS is FUN');
+
+console.log(40+8+23-10);
+
+
+// Value, Variables
+console.log('Jonas');
+
+let firstName = "Seungjo";
+let lastName = "Han";
+console.log(firstName + lastName)
+
+let PI = 3.141592; // 값이 아예 고정이면 upper case 로 쓰는 편이다.
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
