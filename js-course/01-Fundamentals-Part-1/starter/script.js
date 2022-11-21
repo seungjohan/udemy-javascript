4f1a68ce94e0a0be06235ef5316c3b6229247440
seungjohan <cmostrillion@gmail.com>
2022-11-21 21:02:53 +0900

    [study] fundamental grammer of js, section 2 finished

--- js-course/01-Fundamentals-Part-1/starter/script.js
+++ js-course/01-Fundamentals-Part-1/starter/script.js
@@ -130,7 +130,7 @@ multiple
 lines`);
 */
 
-
+/*
 const age = 17;
 // const isOldEnough = age >= 19;
 
@@ -152,4 +152,141 @@ if(birthYear <= 2000){
     century = 21;
 }
 console.log(century);
+*/
 
+/*
+// type conversion
+const inputYear = '1966';
+console.log(Number(inputYear), inputYear); // convert from string to number by using this function
+console.log(inputYear + 18);
+
+console.log(Number('Jonas')); // error -> NaN, not a number = invalid number
+console.log(typeof NaN); // type is number, It's just invalid number
+
+// type coercion
+console.log('I am' + 25 + 'years old' ) // + is a trigger a coercion to strings.
+console.log('23' - '10' - '3'); // 10
+console.log('23' * 2); // 46
+
+let n = '1' + 1; // string, number, + operator -> 11
+n  = n - 1; // converted to a number 
+console.log(n);
+*/
+
+/*
+// Truthy and Falsy Values
+console.log(Boolean(0)); // false
+console.log(Boolean(undefined)); // false
+console.log(Boolean('Jonas')); // true
+console.log(Boolean({})); // true
+console.log(Boolean(' ')); // false
+
+const money = 0;
+if(money) {
+    console.log("Don't spend it all");
+} else {
+    console.log("You should ge a job!");
+}
+
+let height;
+if(height) {
+    console.log("YAY! height is defined!");
+} else {
+    console.log("Undefined!");
+}
+*/
+
+/*
+// Equality operators ==, ===
+const age = '19';
+if(age === 19) console.log('You just become an adult!'); // false
+if(age == 19) console.log('You just become an adult!'); // true
+
+const favourite1 = prompt("What's your favourite1 number?");
+console.log(favourite1);
+console.log(typeof(favourite1)); // string
+const favourite = Number(prompt("What's your favourite number?")); // number
+
+// if(favourite == 23){ // '23' == 23
+if(favourite === 23){
+    console.log("cool! 23 is an amazing number!");
+} else if(favourite === 7) {
+    console.log("7 is also a cool number");
+} else {
+    console.log("It's not that cool except 23 or 7");
+}
+
+if(favourite !== 23) console.log("Why not 23?");
+*/
+
+/*
+//Logical Operators
+const hasDriverLicense = true; // A
+const hasGoodVision = false; // B
+
+console.log(hasDriverLicense && hasGoodVision); // AND
+console.log(hasDriverLicense || hasGoodVision); // OR
+console.log(!hasDriverLicense); // NOT
+
+const shouldDrive = hasDriverLicense && hasGoodVision;
+
+// if(shouldDrive){
+if(hasDriverLicense && hasGoodVision){
+    console.log('Sarah is able to drive');
+} else{
+    console.log('Someone else should drive');
+}
+
+const isTired = true;
+console.log(hasDriverLicense && hasGoodVision && isTired); 
+*/
+ 
+/*
+//switch
+const day = 'monday';
+
+switch(day) {
+    case 'monday': // day === 'monday'
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log("Prepare video");
+        break
+    case 'wednesday':
+    case 'thursday':
+        console.log('Tomorrow is Friday');
+        break
+    case 'friday':
+        console.log("Good job for this weekdays");
+        break
+    case 'saturday':
+        console.log("Prepare video");
+        break
+    case 'sunday':
+        console.log("Fuck Monday");
+        break
+    default:
+        console.log('Not a valid day');
+}
+*/
+
+/*
+//statement, expressions
+if (23 > 10){
+    const str = '23 is bigger'; // statement , expression
+}
+
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1997} years old ${me}`); // we need to put the expression inside.
+*/
+
+/*
+//conditional operator
+const age = 23;
+age >= 18 ? console.log('I like to drink wine') : console.log('water...'); 
+
+const drink = age >= 18 ? 'wine' : 'water'; // expression
+console.log(drink);
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);
+*/
